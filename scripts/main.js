28096884438ee69ed04424161726e0111c190278
arcum-omni <travis.l.eiler@gmail.com>
2019-11-07 12:53:32 -0800

    Uncommited Changed from Last Week

--- scripts/main.js
+++ scripts/main.js
@@ -9,16 +9,51 @@ var ToDoItem = /** @class */ (function () {
     return ToDoItem;
 }());
 /* Write Test Code */
-var myItem = new ToDoItem("Learn about cookies !@#$%=;");
-myItem.isComplete = false;
-// October 29th, 2019: Months starts at 0
-myItem.deadline = new Date(2019, 9, 29);
-// stringify converts any object into a JavaScrip Object Notation (JSON) string format
-var strData = JSON.stringify(myItem);
-console.log(strData);
-// Setting cookie called 'toDoItems' that expires 90 days
-Cookies.set("toDoItems", strData, { expires: 90 });
+// let myItem = new ToDoItem("Learn about cookies !@#$%=;");
+// myItem.isComplete = false;
+// // October 29th, 2019: Months starts at 0
+// myItem.deadline = new Date(2019, 9, 29);
+//
+// // stringify converts any object into a JavaScrip Object Notation (JSON) string format
+// let strData = JSON.stringify(myItem);
+// console.log(strData);
+//
+// const cookieKey = "todoitems";
+// // Setting cookie called 'toDoItems' that expires 90 days
+// Cookies.set(cookieKey, strData, {expires: 90});
+//
+// // Read ToDoItem from cookie
+// let cookieItem:ToDoItem = JSON.parse(Cookies.get(cookieKey));
+// console.log("read cookie data");
+// console.log(cookieItem.title + " " + cookieItem.deadline);
+//
+// // TODO:  Store ToDo item using HTML5 Web Storage
+// //localStorage.setItem("task", strData);
+// const storageKey = "task";
+// if (typeof(Storage) != "undefined") {
+//     localStorage.setItem(storageKey, strData);
+//     let storageStr = localStorage.getItem(storageKey);
+//     let item:ToDoItem = JSON.parse(storageStr);
+//     console.log("read local storage");
+//     console.log(item.title + " " + item.deadline);
+// }
 /* End Test Code */
+/* Use local storage instead of cookies */
+var storageKey = "MyItems";
+function saveToDoItems(items) {
+    localStorage.setItem(storageKey, JSON.stringify(items));
+    // let stringData = JSON.stringify(items);
+    // localStorage.setItem("MyItems", stringData);
+}
+function readToDoItems() {
+    var strData = localStorage.getItem(storageKey);
+    if (strData == null) {
+        return new Array();
+    }
+    return JSON.parse(strData);
+    // let itemArr:Array<ToDoItem> = JSON.parse(stringData);
+    // return itemArr;    
+}
 window.onload = function () {
     var addBtn = document.querySelector("form > input[type=button]");
     addBtn.onclick = main;
@@ -31,7 +66,12 @@ function main() {
     var item = getItem();
     // display new todo item on page
     displayToDoItem(item);
-    // save todo item
+    var allItems = readToDoItems();
+    allItems.push(item); // Add new item to existing list
+    saveToDoItems(allItems);
+    for (var i = 0; i < allItems.length; i++) {
+        alert(allItems[i].title);
+    }
 }
 /**
  * Move selected task to completed section of the page
--- typescripts/main.ts
+++ typescripts/main.ts
@@ -16,19 +16,73 @@ class ToDoItem{
 
 /* Write Test Code */
 
-let myItem = new ToDoItem("Learn about cookies !@#$%=;");
-myItem.isComplete = false;
-// October 29th, 2019: Months starts at 0
-myItem.deadline = new Date(2019, 9, 29);
-
-// stringify converts any object into a JavaScrip Object Notation (JSON) string format
-let strData = JSON.stringify(myItem);
-console.log(strData);
-
-// Setting cookie called 'toDoItems' that expires 90 days
-Cookies.set("toDoItems", strData, {expires: 90});
+    // let myItem = new ToDoItem("Learn about cookies !@#$%=;");
+    // myItem.isComplete = false;
+    // // October 29th, 2019: Months starts at 0
+    // myItem.deadline = new Date(2019, 9, 29);
+    //
+    // // stringify converts any object into a JavaScrip Object Notation (JSON) string format
+    // let strData = JSON.stringify(myItem);
+    // console.log(strData);
+    //
+    // const cookieKey = "todoitems";
+    // // Setting cookie called 'toDoItems' that expires 90 days
+    // Cookies.set(cookieKey, strData, {expires: 90});
+    //
+    // // Read ToDoItem from cookie
+    // let cookieItem:ToDoItem = JSON.parse(Cookies.get(cookieKey));
+    // console.log("read cookie data");
+    // console.log(cookieItem.title + " " + cookieItem.deadline);
+    //
+    // // TODO:  Store ToDo item using HTML5 Web Storage
+    // //localStorage.setItem("task", strData);
+    // const storageKey = "task";
+    // if (typeof(Storage) != "undefined") {
+    //     localStorage.setItem(storageKey, strData);
+    //     let storageStr = localStorage.getItem(storageKey);
+    //     let item:ToDoItem = JSON.parse(storageStr);
+    //     console.log("read local storage");
+    //     console.log(item.title + " " + item.deadline);
+    // }
 
 /* End Test Code */
+
+/* Use local storage instead of cookies */
+
+const storageKey = "MyItems";
+
+function saveToDoItems(items:Array<ToDoItem>){
+    localStorage.setItem(storageKey, JSON.stringify(items));
+        // let stringData = JSON.stringify(items);
+        // localStorage.setItem("MyItems", stringData);
+}
+
+function readToDoItems():Array<ToDoItem>{
+    let strData = localStorage.getItem(storageKey);
+
+    if(strData == null){
+        return new Array<ToDoItem>();
+    }
+    return <Array<ToDoItem>>JSON.parse(strData)
+        // let itemArr:Array<ToDoItem> = JSON.parse(stringData);
+        // return itemArr;    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 window.onload = function(){
@@ -47,7 +101,13 @@ function main(){
     // display new todo item on page
     displayToDoItem(item);
 
-    // save todo item
+    let allItems = readToDoItems();
+    allItems.push(item);  // Add new item to existing list
+    saveToDoItems(allItems);
+
+    for(let i=0; i<allItems.length; i++){
+        alert(allItems[i].title);
+    }
 }
 
 
